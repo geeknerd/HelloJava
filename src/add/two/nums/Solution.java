5e0e93831e4ba4cb3cf21490dbbdc16dc9e71a99
Yi Xia <yxia921@gmail.com>
2015-10-22 21:51:16 -0500

    Updated java doc

--- src/add/two/nums/Solution.java
+++ src/add/two/nums/Solution.java
@@ -5,7 +5,18 @@ import java.util.Stack;
 
 //  Definition for singly-linked list.
 
- 
+/**
+ * You are given two linked lists representing two non-negative numbers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * 
+ * @author Yi Xia
+ * @date Oct 22, 2015
+ *
+ */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     	Stack<ListNode> s1 = new Stack<ListNode>();
